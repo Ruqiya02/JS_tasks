022bcbac223aa3e2d2af01cc660d547e4460b1e1
Ruqiya02 <heyderliruqiyye2@gmail.com>
2024-08-29 07:59:36 +0400

    first

---
+++ index.js
@@ -0,0 +1,17 @@
+// _____TASK-1______
+// Bir 'myArray' adli deyisen yaradin ve her hansi bir arrayi menimsedin .
+// Algoritma girilən arrayin içindəki elementlərin hər birindən neçə dənə olduğunu ekrana yazdırmalıdır.
+
+/*let arr=[1,5,7,90,5,1,2,1,7,'salam', 'hello', 'salam'];
+console.log(arr)
+for(let i=0; i<arr.length; i++){
+    let counter=0;
+    for(let j=0; j<arr.length; j++){
+        if(arr[i]===arr[j]){
+            counter++
+        }
+    }
+    console.log(arr[i]+" => " + counter+" eded")
+}*/
+
+// _____TASK-2______
