eb34cad218c7e30f1320d2d5e92143970517c08d
Ruqiya02 <heyderliruqiyye2@gmail.com>
2024-08-29 09:03:05 +0400

    second push

--- index.js
+++ index.js
@@ -15,3 +15,36 @@ for(let i=0; i<arr.length; i++){
 }*/
 
 // _____TASK-2______
+// 'firstArr' ve 'secondArr' adli iki deyisken yaradib, her hansi massivi menimsedin.
+// Algoritma göndərilən iki arrayi təkrardırıb içindəki elementlərin eyni olub olmadığını
+//  yoxlamalı və ona görə boolean dəyər ekrana çap çap.
+/*function arraysEqual(firstArr, secondArr){
+    firstArr.sort();
+    secondArr.sort();
+    if (firstArr.length !== secondArr.length) {
+        return false;
+    }
+    for(let i=0; i<firstArr.length; i++){
+        if(firstArr[i]!==secondArr[i]){
+        return false;
+        }
+    }
+    return true;
+}
+let firstArr=[2,3,5,"hello"]
+let secondArr=[5,3,2,"hello"]
+let result=arraysEqual(firstArr, secondArr)
+console.log(firstArr, secondArr, result)*/
+
+// _____TASK-3______
+// Bir nömrə arrayı yaradın və buna dəyər mənimsədin.
+// Arrayin içində olan ən böyük ikinci və ən kiçik ikinci ədədləri ekrana yazdı
+/*var arr=[]
+for(let i=0;i<10;i++){
+    var randomItem=parseInt(Math.random()*100)
+    arr.push(randomItem)
+}
+let sortedArr=arr.sort()
+console.log(sortedArr)
+console.log(sortedArr[sortedArr.length-2])*/
+
